54b8dfc1ecf3eb178352665b28d4646d0f4301e0
Magnus N. <143735701+Magnewei@users.noreply.github.com>
2024-10-24 11:18:59 +0200

    rip tests

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -6,7 +6,7 @@ import dat.config.Populate;
 public class Main {
 
     public static void main(String[] args) {
-        /* Populate.main(args); */
+        Populate.main(args);
         ApplicationConfig.startServer(7070);
     }
 }
