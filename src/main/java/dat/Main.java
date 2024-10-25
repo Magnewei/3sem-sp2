2e93efdbad75b00088d455447c06b8a7ba22d2a6
Magnus N. <143735701+Magnewei@users.noreply.github.com>
2024-10-25 13:50:40 +0200

    Collection fix

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -6,8 +6,9 @@ import dat.config.Populate;
 public class Main {
 
     public static void main(String[] args) {
-        Populate.main(args);
+
         ApplicationConfig.startServer(7070);
         //HEJ
+        //Populate.main(args);
     }
 }
